c68800e5b4d44059c153638ea74474a62b7646ed
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-01-21 10:42:43 +0700

    Update expertise.js

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -2,7 +2,7 @@ const expertise = [
     {
         id: 0,
         title: 'Full Stack Development',
-        desc: 'I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients.',
+        desc: 'I am a skilled H4N stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients.',
     },
     {
         id: 1,
@@ -12,7 +12,7 @@ const expertise = [
     {
         id: 2,
         title: 'Designing (UI/UX)',
-        desc: 'I am proficient in using design software such as Figma, Adobe XD, and Adobe Illustrator. Through participating in hackathons and freelance work, I have gained practical experience in UI/UX design and graphic design. Designing is my passion, and it has allowed me to continue to develop my creative skills and grow in the field.',
+        desc: 'I am proficient in using design software such as Pixellab, Alight Motion, and More. Through participating in hackathons and freelance work, I have gained practical experience in UI/UX design and graphic design. Designing is my passion, and it has allowed me to continue to develop my creative skills and grow in the field.',
     },
     {
         id: 3,
