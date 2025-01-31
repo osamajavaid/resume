8d63755ee63952ee7b50e4af2e2c16384d7c0ec0
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-01-31 07:31:51 +0700

    Update background.js

--- pages/api/background.js
+++ pages/api/background.js
@@ -4,23 +4,23 @@
             {
                 "id": 0,
                 "title": "Universitas Lampung (UNILA)",
-                "degree": "S2, Magister Teknik Informatika",
+                "degree": "Junior, Magister Teknik Informatika",
                 "detail": "Gelar Magister Teknik Informatika dari Universitas Lampung (UNILA) dengan fokus pada Pengembangan Aplikasi Chatbot dan Kecerdasan Buatan.",
                 "year": "2022-2024"
             },
             {
                 "id": 1,
                 "title": "Institut Teknologi Sumatera (ITERA)",
-                "degree": "S1, Teknik Informatika",
+                "degree": "Junior, Teknik Informatika",
                 "detail": "Gelar Sarjana Teknik Informatika dari Institut Teknologi Sumatera (ITERA) dengan spesialisasi dalam Pengembangan Perangkat Lunak dan Sistem Terdistribusi.",
                 "year": "2017-2021"
             },
             {
                 "id": 2,
-                "title": "SMA Negeri 2 Bandar Lampung",
-                "degree": "SMA, Jurusan IPA",
-                "detail": "Lulusan SMA Negeri 2 Bandar Lampung dengan jurusan Ilmu Pengetahuan Alam (IPA).",
-                "year": "2014-2017"
+                "title": "SMK Muhammadiyah 1 Kota Agung",
+                "degree": "SMA, Jurusan BP",
+                "detail": "Lulusan SMK Muhammadiyah 1 Kota Agung dengan jurusan Broadcasting  (BP).",
+                "year": "2024-Present"
             }
         ]
     },
@@ -31,7 +31,7 @@
                 "title": "Tensura Interactive",
                 "role": "Lead WhatsApp Bot Developer",
                 "url": "https://tensura-interactive.com",
-                "desc": "Memimpin pengembangan bot WhatsApp untuk klien korporat, menggunakan Node.js, Twilio API, dan integrasi dengan sistem CRM.",
+                "desc": "Memimpin pengembangan bot WhatsApp untuk klien korporat, menggunakan Node.js, Twilio API, dan integrasi dengan sistem database MongoDB",
                 "year": "03/2023 - Present",
                 "location": "Bandar Lampung, Indonesia"
             },
@@ -50,7 +50,7 @@
                 "role": "Bot Developer",
                 "url": "https://developers.facebook.com/",
                 "desc": "Mengembangkan bot Facebook Messenger untuk otomatisasi layanan pelanggan dan pemasaran, menggunakan Facebook Graph API dan Dialogflow.",
-                "year": "08/2020 - 05/2021",
+                "year": "08/2020 - 05/2023",
                 "location": "Bandar Lampung, Indonesia"
             }
         ]
